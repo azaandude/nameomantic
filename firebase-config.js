4572878222c6860e9ca8451a94e0057080280d52
azaandude <azaanyousaf@gmail.com>
2026-04-26 10:10:38 +0000

    Finalizing Firebase and launch

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// NAME-O-MATIC 9000 — Firebase Config
+var FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyBUxLBhI8dHqeJ_O8wzvNLCcn2BK15jg6c",
+  authDomain:        "nameomantic.firebaseapp.com",
+  projectId:         "nameomantic",
+  storageBucket:     "nameomantic.firebasestorage.app",
+  messagingSenderId: "263103610356",
+  appId:             "1:263103610356:web:5010f92c957ee9bfd0cda1",
+  measurementId:     "G-1J7S8KMWCB"
+};
